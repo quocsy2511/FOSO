0e73f95bc121a98df905fd88e4bf3849e2e5fe88
nguyễn quốc sỹ <quocsy2511@gmail.com>
2025-03-26 13:46:36 +0700

    FOSO

--- src/components/rate/Rating.tsx
+++ src/components/rate/Rating.tsx
@@ -1,10 +1,6 @@
 import React, { ReactNode } from "react";
-
+import { motion } from "framer-motion";
 type ReactBlogProps = {
-  // count: number;
-  // title: string;
-  // icon: ReactNode;
-
   title: string;
   icon: ReactNode;
   isSelected: boolean;
@@ -13,21 +9,33 @@ type ReactBlogProps = {
 
 const ReactBlog = ({ icon, title, isSelected, onClick }: ReactBlogProps) => {
   return (
-    <div
-      // className="rounded-[4px] gap-y-1 flex flex-col py-1 px-[25px] items-center justify-center border border-transparent hover:border-backgroundColor-green-5 hover:text-backgroundColor-green-5 text-typo-gray-1"
+    <motion.div
       className={`rounded-[4px] gap-y-1 flex flex-col py-1 px-[25px] items-center justify-center border border-transparent 
-        hover:border-backgroundColor-green-5 hover:text-backgroundColor-green-5 text-typo-gray-1 cursor-pointer 
-        ${
-          isSelected
-            ? "text-backgroundColor-green-5 border-backgroundColor-green-5"
-            : ""
-        }`}
+       hover:border-backgroundColor-green-5 hover:text-backgroundColor-green-5 text-typo-gray-1 cursor-pointer 
+       ${
+         isSelected
+           ? "text-backgroundColor-green-5 border-backgroundColor-green-5"
+           : ""
+       }`}
       onClick={onClick}
+      whileHover="hover"
     >
-      {icon}
+      <motion.div
+        variants={{
+          hover: { y: [-5, 0, -5] },
+        }}
+        transition={{
+          duration: 0.4,
+          repeat: Infinity,
+          repeatType: "reverse",
+        }}
+      >
+        {icon}
+      </motion.div>
+
       <span className="font-bold">{isSelected ? 1 : 0}</span>
       <p className="font-medium whitespace-nowrap">{title}</p>
-    </div>
+    </motion.div>
   );
 };
 
