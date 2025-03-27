07283801973895ac6440851c2d3d74be6249fe4e
nguyễn quốc sỹ <quocsy2511@gmail.com>
2025-03-27 19:29:47 +0700

    FOSO

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -684,6 +684,7 @@ const Blog = () => {
           initial={{ x: -100, opacity: 0 }}
           animate={{ x: 0, opacity: 1 }}
           transition={{ duration: 0.8, ease: "easeOut" }}
+          className="3xl:block hidden"
         >
           <Social />
         </motion.div>
